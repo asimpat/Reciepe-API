ad0c6de58bed27c995df501a7e156affdf3e937a
Asim <asimokon@yahoo.com>
2025-10-27 23:13:11 +0100

    modifed settings

--- api/settings.py
+++ api/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'recipe',
-    'users',
+    'users', 
     'interactions',
     'notifications',
     'rest_framework_simplejwt', 
