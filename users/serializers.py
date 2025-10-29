49cdcf1b13b92c7b7ee0412350f35dcf6c53f51b
Asim <asimokon@yahoo.com>
2025-10-29 14:01:09 +0100

    added user views

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,3 @@
-from users.serializers import UserSerializer
-from .models import Recipe
 from rest_framework import serializers
 from django.contrib.auth.password_validation import validate_password
 from .models import User
