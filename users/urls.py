20bfc9c6b70d4af54b62df66f20c16afef12e719
Asim <asimokon@yahoo.com>
2025-11-06 17:00:00 +0100

    added following users fields

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import RegisterView, LoginView, ProfileView, UserDetailView
+from .views import (RegisterView, LoginView, ProfileView, UserDetailView,
+                    FollowUserView, FollowersListView, FollowingListView)
 
 
 urlpatterns = [
@@ -10,5 +11,10 @@ urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('<str:username>/', UserDetailView.as_view(), name='user-detail'),
+    path('<str:username>/follow/', FollowUserView.as_view(),
+         name='follow-user'),
+    path('<str:username>/followers/', FollowersListView.as_view(),
+         name='followers-list'),
+    path('<str:username>/following/',
+         FollowingListView.as_view(), name='following-list')
 ]
- 
