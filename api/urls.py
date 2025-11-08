e5e1251dd0826e9a413f9705460d2d2f6ded287e
Asim <asimokon@yahoo.com>
2025-11-08 22:21:49 +0100

    added swaggar doc

--- api/urls.py
+++ api/urls.py
@@ -21,4 +21,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+                          document_root=settings.MEDIA_ROOT) 
