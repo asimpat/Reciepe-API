7cefe87149e570fea751786ad32fe5c10cf10211
Asim <asimokon@yahoo.com>
2025-11-09 22:24:39 +0100

    made correct in settings

--- api/settings.py
+++ api/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-placeholder')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = []
-  .l
+
 
 # Application definition
 INSTALLED_APPS = [
